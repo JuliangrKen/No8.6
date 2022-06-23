788b58b4fa30ee13f2a7b562b268a808ceb6e61b
Juliandr Kendzikundu D'Amar Hoyamovich III <vladik-g2911shangyang@mail.ru>
2022-06-23 23:20:44 +0300

    dad

---
+++ Task2/MyTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class MyTask
+{
+    public static long GetDirSize(string dirURL)
+    {
+        DirectoryInfo directory;
+
+        try
+        {
+            directory = new DirectoryInfo(dirURL);
+        }
+        catch
+        {
+            Console.WriteLine("Некорректная URL дирректории!");
+            return 0;
+        }
+
+        long size = 0;
+
+        try // И без нижних try работает нормально, но пусть лучше будут, а то мало ли xD
+        {
+            foreach (var file in directory.GetFiles())
+                size += file.Length;
+        }
+        catch { }
+
+        try
+        {
+            foreach (DirectoryInfo dir in directory.GetDirectories())
+                size += GetDirSize(dir.FullName);
+        }
+        catch { }
+
+        return size;
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// #2
+// Напишите программу, которая чистит нужную нам папку от файлов  и папок, которые не использовались более 30 минут.
+
+Console.Write("Введите полный путь до папки: ");
+var path = Console.ReadLine();
+if (path != null)
+    Console.WriteLine(MyTask.GetDirSize(path));
